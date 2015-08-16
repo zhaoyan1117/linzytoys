2c27b5ff70c6aa2732706725503de2e1635bb33c
Yan Zhao <zhaoyan1117@gmail.com>
2015-08-16 12:47:43 -0700

    checkout page specific

--- embedded.js
+++ embedded.js
@@ -1,1 +1,6 @@
-alert('haha!');
+var onCheckoutPage = location.pathname == "/one-page-checkout.asp") ||
+  (location.pathname.indexOf("/one-page-checkout.asp") != -1);
+
+if (onCheckoutPage) {
+  alert('haha');
+}
