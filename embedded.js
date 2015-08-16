377d59c2cf14c41f645c78d66e00757f055c2999
Yan Zhao <zhaoyan1117@gmail.com>
2015-08-16 12:53:22 -0700

    wup

--- embedded.js
+++ embedded.js
@@ -1,4 +1,4 @@
-var onCheckoutPage = location.pathname == "/one-page-checkout.asp") ||
+var onCheckoutPage = location.pathname == "/one-page-checkout.asp") \
   (location.pathname.indexOf("/one-page-checkout.asp") != -1);
 
 if (onCheckoutPage) {
