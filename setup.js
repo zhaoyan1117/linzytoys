40eeda584dcbc7688675d3bb6ddf8b25ddbd27fb
Yan Zhao <zhaoyan1117@gmail.com>
2015-09-03 21:51:39 -0700

    add get url parameter

--- setup.js
+++ setup.js
@@ -17,6 +17,11 @@ var util = {
             cur = cur || (curLoc == curPath) || (curLoc.indexOf(curPath) != -1);
         }
         return cur;
+    },
+
+    // http://stackoverflow.com/questions/11582512/how-to-get-url-parameters-with-javascript/11582513#11582513
+    getURLParameter: function(name) {
+        return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
     }
 }
 
